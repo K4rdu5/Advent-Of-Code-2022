113a9fd3459af6802a10de7223b52c9ec63f3804
Lucas <87875365+K4rdu5@users.noreply.github.com>
2022-12-07 17:20:42 +0100

    Create Day.py

---
+++ Day7/Day.py
@@ -0,0 +1,28 @@
+#Day 7
+from collections import defaultdict
+
+with open("day7.txt", "r") as f:
+    data = [line for line in f.read().split("\n")]
+
+path = []
+folder = defaultdict(int)
+for line in data:
+    seg = line.strip().split()
+    if seg[1] == "cd":
+        if seg[2] == "..":
+            path.pop()
+        else:
+            path.append(seg[2])
+    elif seg[0].isdigit():
+        size = seg[0]
+        for i in range(len(path)):
+            print(folder)
+            folder["/".join(path[:i+1])] += int(size)
+print(sum(f for f in folder.values() if f <= 100000))
+
+#Part 2
+used = max(folder.values())
+free = 70000000 - used
+needed = 30000000 - free
+mindel = min([f for f in folder.values() if f >= needed])
+print(mindel)
